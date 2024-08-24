1c4925480134fd7225b9cc895438b10608da50bf
arbrown09 <arbrown.profit09@gmail.com>
2024-08-23 21:36:46 -0700

    Add files via upload

--- gross_pay.py
+++ gross_pay.py
@@ -1,21 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[9]:
-
-
-#get user's hourly rate of pay
-rate=float(input("How much do you make an hour?: " ))
-#get user's hours worked
-hours=float(input("How many hours did you work this week?: "))
-#calculate weekly pay
-pay= rate * hours
-#output message
+# get user's hourly rate of pay
+rate = float(input("How much do you make an hour?: "))
+# get user's hours worked
+hours = float(input("How many hours did you work this week?: "))
+# calculate weekly pay
+pay = rate * hours
+# output message
 print(f"Your weekly pay is: ${pay:.2f}")
-
-
-# In[ ]:
-
-
-
-
